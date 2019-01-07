828514a55c56898862c30482b684dd6b076f5512
natashabuck <natashabuck1@gmail.com>
2019-01-07 10:31:42 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    if (action.type === 'ADD_FRIEND'){
+        
+        return {friends: [...state.friends, action.friend]};
+    }
+    else if (action.type === 'REMOVE_FRIEND'){
+        return {friends: [...state.friends.filter(f => f.id !== action.id)]}
+    }
+    
+    else {
+        return state;
+    }
 }
